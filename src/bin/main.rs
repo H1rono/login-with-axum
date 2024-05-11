fe0f272847acacc9316a96c1ffb56d4f6cc9f267
H1rono <hronok66@gmail.com>
2024-05-12 06:08:52 +0900

    Impl main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,19 @@
+use tracing_subscriber::EnvFilter;
+
 use login_with_axum as lib;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    let app = lib::make_router();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or("info".into()))
+        .init();
+    let options = lib::db_options_from_env("MYSQL_")
+        .or_else(|_| lib::db_options_from_env("MARIADB_"))
+        .or_else(|_| lib::db_options_from_env("NS_MARIADB_"))?;
+    let repo = lib::Repository::connect_with(options).await?;
+    repo.migrate().await?;
+    let app_state = lib::AppState::new(repo);
+    let app = lib::make_router(app_state).await;
     let port: u16 = std::env::var("PORT")
         .unwrap_or_else(|_| "4176".to_string())
         .parse()?;
