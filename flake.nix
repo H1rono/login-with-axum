8596f730466c6f31a79d0d6ddd8d3ecb317c399a
H1rono <hronok66@gmail.com>
2024-05-11 13:04:30 +0900

    Init nix flakes

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Just login (with axum)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+        inherit (pkgs) lib;
+        toolchain = pkgs.fenix.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-opUgs6ckUQCyDxcB9Wy51pqhd0MPGHUVbwRKKPGiwZU=";
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = toolchain;
+          cargo = toolchain;
+        };
+      in
+      {
+        packages.default = rustPlatform.buildRustPackage {
+          pname = "login-with-axum";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ ];
+          buildInputs = [ ];
+          doCheck = false;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ toolchain ];
+        };
+      });
+}
