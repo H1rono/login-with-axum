e67dfe1f21884fa0aeb644c5f0359d81d097d8e1
H1rono <hronok66@gmail.com>
2025-02-21 13:26:53 +0900

    :fire: Remove comments

--- migrations/0_create-table.sql
+++ migrations/0_create-table.sql
@@ -8,6 +8,3 @@ CREATE TABLE IF NOT EXISTS `user_passwords` (
     `user_id` BINARY(16) NOT NULL PRIMARY KEY,
     `psk` VARCHAR(128) NOT NULL
 );
-
--- session sotre `user_sessions` is created via async-sqlx-session:
--- https://github.com/jbr/async-sqlx-session/blob/06a3abb8941edaea3d3e8133c30ee16231914a25/src/mysql.rs#L148-L168
